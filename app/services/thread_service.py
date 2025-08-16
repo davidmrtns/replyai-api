59856c43582f65d5cfd1c9233d2e674da1f63fac
davidmrtns <daviddmrtns@gmail.com>
2025-08-15 22:29:02 -0300

    fix: fix execute_thread method to use auxiliary method to assign threads to contacts

--- app/services/thread_service.py
+++ app/services/thread_service.py
@@ -28,14 +28,7 @@ async def execute_thread(
     response, thread_id = assistant.criar_rodar_thread(thread_id=current_thread_id)
 
     if not contact.current_thread:
-        thread = Thread(
-            thread_id=thread_id,
-            last_message_from="assistant",
-            contact_id=contact.id
-        )
-        db.add(thread)
-        contact.current_thread = thread
-        db.commit()
+        assign_new_thread_to_contact(contact, thread_id, db)
 
     response = json.loads(response)
     response_obj = Resposta.from_dict(response)
