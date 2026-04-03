7eadfa9fecfeea3a7ba741e3543c72b8ad8be79d
davidmrtns <daviddmrtns@gmail.com>
2026-04-03 18:19:59 -0300

    feat: adjust entrypoint to allow both api and worker start up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-./scripts/generate_database.sh && exec uvicorn main:app --host 0.0.0.0 --port 8000
+set -e
+
+if [ "$APP_MODE" = "api" ]; then
+    ./scripts/generate_database.sh
+    echo "Starting API server..."
+    exec uvicorn main:app --host 0.0.0.0 --port 8000
+
+elif [ "$APP_MODE" = "worker" ]; then
+    echo "Starting worker..."
+    exec python -m app.queue.worker
+
+else
+    echo "Invalid APP_MODE: $APP_MODE. Use 'api' or 'worker'."
+    exit 1
+fi
